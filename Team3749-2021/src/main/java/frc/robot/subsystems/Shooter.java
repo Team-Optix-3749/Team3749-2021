042a90215fa142ca6c26f2ad49a12acc433c9908
Aadit Gupta <spacegiant21@gmail.com>
2020-10-27 16:35:16 -0700

    imports

--- Team3749-2021/src/main/java/frc/robot/subsystems/Shooter.java
+++ Team3749-2021/src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,6 +1,13 @@
 package frc.robot.subsystems;
 
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.ControlType;
 
 public class Shooter extends SubsystemBase {
   /**
