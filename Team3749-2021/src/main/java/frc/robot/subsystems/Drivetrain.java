0f2d84fbe2d789c37bc639e275c2eb9be7d422f3
Rebecca-123 <rebecca.notw@gmail.com>
2020-11-09 07:48:15 -0800

    first drivetrain commit

---
+++ Team3749-2021/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+  
+}
