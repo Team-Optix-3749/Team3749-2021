85c53caa7f34fd2fb5a678084acf4141bfe9316d
Rohan Juneja <rohanj2006@gmail.com>
2020-11-08 19:39:29 -0800

    Finished Max, Set, Stop Commands for Shooter (#16)
    * add pranavs lit port maps
    
    * Create Subsytem, Methods, and Commands
    
    * imports
    
    * Add SparkMax Dependencies
    
    * getter and stuff for constants also made robotcontainer static
    
    * Add 2020 Code from Shooter Subsystem
    
    * Shorten Constants into classes
    
    * Add Comments to Constants
    
    * Adjust for new constant class
    
    * Make constants public
    
    * Remove getConstants, use static class
    
    * comments
    
    * Annotate Methods
    
    * Add constructor comment
    
    * Make all the Constants Final
    
    * Add Shooter Gains
    
    * Use PID and Velocity for Shooter
    
    * Add javadoc param to set method
    
    * Start Working on Shoot Command
    
    * Use Double Supplier for controller input
    
    * Stop Shooter when Shoot Command Finished
    
    * Update Shooter Constants
    
    * setOrbit
    
    * ShootMax
    
    * change param
    
    * javadoc fixes
    
    Co-authored-by: raad1masum <piraadwan@gmail.com>
    Co-authored-by: Aadit Gupta <spacegiant21@gmail.com>
    Co-authored-by: Raadwan Masum <39575185+raad1masum@users.noreply.github.com>

---
+++ Team3749-2021/src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,73 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.ControlType;
+
+/**
+ * Shooter Subsystem
+ *
+ * @author Aadit Gupta
+ * @author Rohan Juneja
+ */
+
+public class Shooter extends SubsystemBase {
+  private CANSparkMax m_shooterMotor;
+  private CANEncoder m_encoder;
+  private CANPIDController m_controller;
+
+  /**
+   * Constructor
+   */
+  public Shooter() {
+    m_shooterMotor = new CANSparkMax(Constants.CAN.shooter_motor, MotorType.kBrushless);
+    m_shooterMotor.setIdleMode(IdleMode.kCoast);
+    m_encoder = m_shooterMotor.getEncoder();
+    m_controller = m_shooterMotor.getPIDController();
+    m_controller.setFeedbackDevice(m_encoder);
+    m_controller.setP(Constants.Shooter.kP);
+    m_controller.setI(Constants.Shooter.kI);
+    m_controller.setD(Constants.Shooter.kD);
+    stop();
+  }
+
+  /**
+   * Set Position of Shooter
+   * @param velocity The desired velocity of the shooter
+   */
+
+  public void set(double velocity) {
+    m_controller.setReference(-velocity, ControlType.kVelocity);
+  }
+
+  /**
+   * Stop Shooter
+   */
+
+  public void stop() {
+    m_controller.setReference(0, ControlType.kDutyCycle);
+  }
+
+  /**
+   * Send Powercells to Orbit
+   */
+
+  public void sendToOrbit() {
+    m_controller.setReference(Constants.Shooter.maxVoltage, ControlType.kVoltage);
+  }
+
+  /**
+   * Un-Jam Powercells
+   */
+
+  public void antiJam() {
+
+  }
+}
