47cc7cd28c4c81c8f1f55a6484efd2df2619bd72
YasaswiS <yasaswisingamneni2004@gmail.com>
2020-11-10 14:49:01 -0800

    Initalized motor controller

---
+++ Team3749-2021/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+//import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+
+
+public class Intake extends SubsystemBase {
+
+    private VictorSPX l_intake;
+    private VictorSPX r_intake;
+
+
+    public Intake(){
+
+    }
+
+    public void inward(){
+        
+        l_intake.set(ControlMode.PercentOutput, 0.1);
+        r_intake.set(ControlMode.PercentOutput, -0.1);
+
+    }
+
+    public void outward(){
+
+    l_intake.set(ControlMode.PercentOutput, -0.1);
+    r_intake.set(ControlMode.PercentOutput, 0.1);
+
+    }
+
+
+    public void antiJam(){
+
+    }
+  
+}
