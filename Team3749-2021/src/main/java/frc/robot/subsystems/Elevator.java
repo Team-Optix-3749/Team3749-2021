3edee13002bfd5f1b33f98d89cb8ce0f1d49c958
Akshay Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2020-12-01 15:49:17 -0800

    Elevator (#20)
    * sync elevator with origin/main (#12)
    
    * add programmers and specs for shooter
    
    * Enforce common code style, build sucess, editor configuration (#1)
    
    * Enforce common code style, build sucess, editor configuration
    
    * fix: typo in prettier ci
    
    * fix: prettier ci name(?)
    
    * fix one-letter typo that broke everything
    
    * another one character typo. this commit goes out to aadit
    
    * trigger ci build
    
    * trigger build again???
    
    * trigger on ci branch
    
    * Google Java Format
    
    * fix rgadle build
    
    * add editorconfig lint and fix typo
    
    * remove stupid workflow
    
    * Google Java Format
    
    * move specs to docs
    
    Co-authored-by: github-actions <>
    
    * Integration (#2)
    
    * add pranavs lit port maps
    
    * getter and stuff for constants also made robotcontainer static
    
    * unnecessary files (#4)
    
    * add badge
    
    * Fix Constants File (#5)
    
    * add pranavs lit port maps
    
    * getter and stuff for constants also made robotcontainer static
    
    * Shorten Constants into classes
    
    * Add Comments to Constants
    
    Co-authored-by: raad1masum <piraadwan@gmail.com>
    Co-authored-by: Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
    
    * format is fired (#6)
    
    * Make the Constants Public and Remove Get Constants (#7)
    
    * add pranavs lit port maps
    
    * getter and stuff for constants also made robotcontainer static
    
    * Shorten Constants into classes
    
    * Add Comments to Constants
    
    * Make constants public
    
    * Remove getConstants, use static class
    
    Co-authored-by: raad1masum <piraadwan@gmail.com>
    Co-authored-by: Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
    
    * Integration (#8)
    
    * add pranavs lit port maps
    
    * getter and stuff for constants also made robotcontainer static
    
    * Shorten Constants into classes
    
    * Add Comments to Constants
    
    * Make constants public
    
    * Remove getConstants, use static class
    
    * no more hashmap, so no more import
    
    Co-authored-by: rjawesome <aonedtop@gmail.com>
    
    * Shooter Gains and Make Constants Final (#9)
    
    * add pranavs lit port maps
    
    * getter and stuff for constants also made robotcontainer static
    
    * Shorten Constants into classes
    
    * Add Comments to Constants
    
    * Make constants public
    
    * Remove getConstants, use static class
    
    * Make all the Constants Final
    
    * Add Shooter Gains
    
    Co-authored-by: raad1masum <piraadwan@gmail.com>
    Co-authored-by: Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
    
    * Tododododododododododo (#10)
    
    * todododoododododododo
    
    * link specs and add emoji
    
    * the space was bad, i am bad (#11)
    
    * add setup for elevator and intake (#15)
    
    Co-authored-by: Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
    Co-authored-by: Aadit Gupta <50186752+aaditgupta21@users.noreply.github.com>
    Co-authored-by: Rohan Juneja <rohanj2006@gmail.com>
    Co-authored-by: raad1masum <piraadwan@gmail.com>
    Co-authored-by: rjawesome <aonedtop@gmail.com>
    
    * added 4 commands, they dont actually do anything yet lol
    
    * stop and start motor for the elevator, i think i did it correctly but prob not
    
    * now they sound :sparkles: nicer :sparkles:
    
    Co-authored-by: Safin Singh <42120904+safinsingh@users.noreply.github.com>
    Co-authored-by: Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
    Co-authored-by: Aadit Gupta <50186752+aaditgupta21@users.noreply.github.com>
    Co-authored-by: Rohan Juneja <rohanj2006@gmail.com>
    Co-authored-by: raad1masum <piraadwan@gmail.com>
    Co-authored-by: rjawesome <aonedtop@gmail.com>

--- Team3749-2021/src/main/java/frc/robot/subsystems/Elevator.java
+++ Team3749-2021/src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,10 +1,25 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class Elevator extends SubsystemBase {
+  private TalonSRX m_elevator;
+
   public Elevator() {
 
+  }
+
+  public void upElevator() {
+    m_elevator.set(ControlMode.PercentOutput, 1);
+  }
+
+  public void downElevator() {
+    m_elevator.set(ControlMode.PercentOutput, -1);
   }
 
   @Override
