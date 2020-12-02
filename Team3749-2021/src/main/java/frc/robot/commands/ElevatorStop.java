3154e6a178a735c5983509b5e238898dc0f3b095
raad1masum <piraadwan@gmail.com>
2020-12-02 08:41:08 -0800

    stop elevator when interrupted

--- Team3749-2021/src/main/java/frc/robot/commands/ElevatorStop.java
+++ Team3749-2021/src/main/java/frc/robot/commands/ElevatorStop.java
@@ -31,7 +31,9 @@ public class ElevatorStop extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_elevator.stopElevator();
+  }
 
   // Returns true when the command should end.
   @Override
