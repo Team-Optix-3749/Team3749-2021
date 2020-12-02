1d81cf65e87a44f473c4b3c642684afb39ab394b
raad1masum <piraadwan@gmail.com>
2020-12-02 08:36:38 -0800

    create stop method for intake

--- Team3749-2021/src/main/java/frc/robot/subsystems/Intake.java
+++ Team3749-2021/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-//import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 
@@ -9,10 +8,6 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 public class Intake extends SubsystemBase {
     private VictorSPX l_intake;
     private VictorSPX r_intake;
-
-    public Intake(){
-
-    }
 
     public void inward(){
       l_intake.set(ControlMode.PercentOutput, 1);
@@ -22,6 +17,11 @@ public class Intake extends SubsystemBase {
     public void outward(){
       l_intake.set(ControlMode.PercentOutput, -1);
       r_intake.set(ControlMode.PercentOutput, 1);
+    }
+    
+    public void stop(){
+      l_intake.set(ControlMode.PercentOutput, 0);
+      r_intake.set(ControlMode.PercentOutput, 0);
     }
 
     public void antiJam(){
