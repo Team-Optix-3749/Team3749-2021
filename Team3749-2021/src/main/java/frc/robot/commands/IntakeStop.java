3f68d6be98286d0714ada8dae2280af561f30853
raad1masum <piraadwan@gmail.com>
2020-12-02 08:41:52 -0800

    stop intake when interrupted

--- Team3749-2021/src/main/java/frc/robot/commands/IntakeStop.java
+++ Team3749-2021/src/main/java/frc/robot/commands/IntakeStop.java
@@ -31,7 +31,9 @@ public class IntakeStop extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_intake.stop();
+  }
 
   // Returns true when the command should end.
   @Override
