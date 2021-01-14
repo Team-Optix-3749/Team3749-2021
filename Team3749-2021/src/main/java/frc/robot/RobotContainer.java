eb7dc29b688a7476a5a1024649e545fd4ad2bc86
rjawesome <aonedtop@gmail.com>
2021-01-13 17:32:27 -0800

    remove unnecessary import

--- Team3749-2021/src/main/java/frc/robot/RobotContainer.java
+++ Team3749-2021/src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,5 @@
 package frc.robot;
 
-import java.util.Arrays;
-
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj2.command.Command;
