b5ac3d6df36c305b0554d67653bf0551e5900654
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2021-01-16 12:38:51 -0800

    Barrel Racing (#33)
    * Add autonomous command into seperate file
    
    * remove unnecessary import
    
    * Variable names/Formatting
    
    * more formatting
    
    * attempt custom trajectory (need to configure robot characterization)
    
    * trajectory tracking auto paths with json import
    
    Co-authored-by: Rohan Juneja <rohanj2006@gmail.com>
    Co-authored-by: spacegiant21@gmail.com <spacegiant21@gmail.com>
    
    * clean up imports and commented code
    
    Co-authored-by: Rohan Juneja <rohanj2006@gmail.com>
    Co-authored-by: spacegiant21@gmail.com <spacegiant21@gmail.com>
    
    * autonomous command 1
    
    * autonomous command 2
    
    * add path weaver config
    
    * remove random print statements we dont need
    
    * run auto command 1 and do full slalom with list of waypoints
    
    * config AutoNav Slalom Path
    
    * add fields
    
    * slalom path works now :muscle:
    
    * slalom path works now :muscle:
    
    * add AutoNav fields
    
    * Add Paths into seperate file
    
    * capitalization conformity
    
    * formatting :100:
    
    * add Galactic Search fields
    
    * optimize return path for slalom
    
    * do not spin after slalom completion
    
    * create constant for auto path
    
    * remove old slalom path
    
    * configure barrel racing path waypoints
    
    Co-authored-by: rjawesome <aonedtop@gmail.com>
    Co-authored-by: Rohan Juneja <rohanj2006@gmail.com>
    Co-authored-by: spacegiant21@gmail.com <spacegiant21@gmail.com>

--- Team3749-2021/src/main/java/frc/robot/Paths/Paths.java
+++ Team3749-2021/src/main/java/frc/robot/Paths/Paths.java
@@ -7,8 +7,7 @@ import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 
 public class Paths {
-
-  public static Path ANSlalom = new Path(
+  public static Path ANSlalomPath = new Path(
     // origin
     new Pose2d(1, 1, new Rotation2d(0)),
     // waypoints
@@ -22,6 +21,27 @@ public class Paths {
       new Translation2d(11, 2),
       new Translation2d(5, 2),
       new Translation2d(3, 3.5)
+    ),
+    // end point
+    new Pose2d(1, 4, new Rotation2d(Math.PI))
+  );
+
+  public static Path ANBarrelRacingPath = new Path(
+    // origin
+    new Pose2d(1, 4, new Rotation2d(0)),
+    // waypoints
+    List.of(
+      new Translation2d(6, 4),
+      new Translation2d(7.5, 2),
+      new Translation2d(6, 2),
+      new Translation2d(6.5, 3.5),
+      new Translation2d(10, 4),
+      new Translation2d(12, 6),
+      new Translation2d(10.5, 6.5),
+      new Translation2d(10.5, 4),
+      new Translation2d(13.5, 2),
+      new Translation2d(13.5, 4)
+      // new Translation2d(6, 3)
     ),
     // end point
     new Pose2d(1, 4, new Rotation2d(Math.PI))
