93977a9da8f24d9293ec83cb77a5a0d7b2ec528f
rjawesome <aonedtop@gmail.com>
2021-02-23 15:50:27 -0800

    add missing comments

--- Team3749-2021/src/main/java/frc/robot/paths/Paths.java
+++ Team3749-2021/src/main/java/frc/robot/paths/Paths.java
@@ -70,15 +70,12 @@ public class Paths {
   );
 
   public static Path GSRedPathB = new Path(
-    // origin
     new Pose2d(0.5, 7.5, new Rotation2d(0)),
-    // waypoints
     List.of(
       new Translation2d(4, 5.2),
       new Translation2d(6.5, 2.8),
       new Translation2d(10, 6)
     ),
-    // end point
     new Pose2d(15, 7.5, new Rotation2d(0))
   );
 
@@ -93,15 +90,12 @@ public class Paths {
   );
 
   public static Path GSBluePathB = new Path(
-    // origin
     new Pose2d(0.5, 1, new Rotation2d(0)),
-    // waypoints
     List.of(
       new Translation2d(7, 2),
       new Translation2d(11, 5.5),
       new Translation2d(13, 3.5)
     ),
-    // end point
     new Pose2d(15, 2, new Rotation2d(0))
   );
 }
