9e58f79f4d0aecba9213e9ae39b34d30832ec798
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2021-03-02 22:25:54 -0800

    added comments to subsystems (#47)
    Co-authored-by: YasaswiS <49824724+YasaswiS@users.noreply.github.com>

--- Team3749-2021/src/main/java/frc/robot/subsystems/Intake.java
+++ Team3749-2021/src/main/java/frc/robot/subsystems/Intake.java
@@ -8,17 +8,18 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 public class Intake extends SubsystemBase {
     private VictorSPX l_intake;
     private VictorSPX r_intake;
-
+   
+   //Start making the intake suck in powercells
     public void inward(){
       l_intake.set(ControlMode.PercentOutput, 1);
       r_intake.set(ControlMode.PercentOutput, -1);
     }
-
+  //reverse speed to move powercells out
     public void outward(){
       l_intake.set(ControlMode.PercentOutput, -1);
       r_intake.set(ControlMode.PercentOutput, 1);
     }
-    
+  //sets speed back to rest at 0 
     public void stop(){
       l_intake.set(ControlMode.PercentOutput, 0);
       r_intake.set(ControlMode.PercentOutput, 0);
