5b1558304d4c1b1c3b2299a4120b689aba0af68b
raad1masum <piraadwan@gmail.com>
2021-03-28 10:36:30 -0700

    fun comments

--- Team3749-2021/src/main/java/frc/robot/subsystems/Intake.java
+++ Team3749-2021/src/main/java/frc/robot/subsystems/Intake.java
@@ -10,18 +10,30 @@ public class Intake extends SubsystemBase {
   private VictorSPX m_intake = new VictorSPX(Constants.CAN.intake_motor);
   private VictorSPX m_lift = new VictorSPX(Constants.CAN.intake_motor_lift);
 
+  /**
+   * Intake power cells in
+   */
   public void intakeIn() {
     m_intake.set(ControlMode.PercentOutput, 1);
   }
 
+  /**
+   * Expel power cells out
+   */
   public void intakeOut() {
     m_intake.set(ControlMode.PercentOutput, -1);
   }
 
+  /**
+   * Lift intake up
+   */
   public void liftUp() {
     m_lift.set(ControlMode.PercentOutput, 1);
   }
 
+  /**
+   * Drop intake down
+   */
   public void liftDown() {
     m_lift.set(ControlMode.PercentOutput, -1);
   }
