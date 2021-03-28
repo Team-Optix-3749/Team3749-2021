79ddc3baad8e73028e9fa1de091bf6d0059b2a45
raad1masum <piraadwan@gmail.com>
2021-03-28 10:33:39 -0700

    create new intake methods for new design

--- Team3749-2021/src/main/java/frc/robot/subsystems/Intake.java
+++ Team3749-2021/src/main/java/frc/robot/subsystems/Intake.java
@@ -7,26 +7,22 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class Intake extends SubsystemBase {
-    private VictorSPX m_intake = new VictorSPX(Constants.CAN.intake_motor);
-    private VictorSPX m_lift = new VictorSPX(Constants.CAN.intake_motor_lift);
-   
-   //Start making the intake suck in powercells
-    public void inward(){
-      l_intake.set(ControlMode.PercentOutput, 1);
-      r_intake.set(ControlMode.PercentOutput, -1);
-    }
-  //reverse speed to move powercells out
-    public void outward(){
-      l_intake.set(ControlMode.PercentOutput, -1);
-      r_intake.set(ControlMode.PercentOutput, 1);
-    }
-  //sets speed back to rest at 0 
-    public void stop(){
-      l_intake.set(ControlMode.PercentOutput, 0);
-      r_intake.set(ControlMode.PercentOutput, 0);
-    }
+  private VictorSPX m_intake = new VictorSPX(Constants.CAN.intake_motor);
+  private VictorSPX m_lift = new VictorSPX(Constants.CAN.intake_motor_lift);
 
-    public void antiJam(){
+  public void intakeIn() {
+    m_intake.set(ControlMode.PercentOutput, 1);
+  }
 
-    }
+  public void intakeOut() {
+    m_intake.set(ControlMode.PercentOutput, -1);
+  }
+
+  public void liftUp() {
+    m_lift.set(ControlMode.PercentOutput, 1);
+  }
+
+  public void liftDown() {
+    m_lift.set(ControlMode.PercentOutput, -1);
+  }
 }
