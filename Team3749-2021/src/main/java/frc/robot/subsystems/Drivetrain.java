c87ba4f589f5e3c30eb26430537b0bf3803792e3
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2021-03-31 18:46:33 -0700

    adjust rotation speeds

--- Team3749-2021/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Team3749-2021/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -199,7 +199,7 @@ public class Drivetrain extends SubsystemBase {
    * @param rot rotation along z-axis
    */
   public void arcadeDrive(double fwd, double rot) {
-    m_drive.arcadeDrive(-fwd * Constants.Drivetrain.kDriveSpeed, rot, true);
+    m_drive.arcadeDrive(-fwd * Constants.Drivetrain.kDriveSpeed, rot * (Constants.Drivetrain.kDriveSpeed - 0.1), true);
   }
 
   /**
@@ -209,7 +209,7 @@ public class Drivetrain extends SubsystemBase {
    * @param rightSpeed speed of right side
    */
   public void tankDrive(double leftSpeed, double rightSpeed) {
-    m_drive.tankDrive(-leftSpeed, -rightSpeed);
+    m_drive.tankDrive(leftSpeed * Constants.Drivetrain.kDriveSpeed, rightSpeed);
   }
 
   /**
