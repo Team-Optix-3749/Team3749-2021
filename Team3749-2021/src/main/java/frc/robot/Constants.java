bd0e05e5abbfd4296392fef9bb05682082ec4d2d
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2021-04-02 23:42:20 -0700

    increase drive speed

--- Team3749-2021/src/main/java/frc/robot/Constants.java
+++ Team3749-2021/src/main/java/frc/robot/Constants.java
@@ -27,8 +27,10 @@ public final class Constants {
   }
 
   public static class Drivetrain {
-    public static final double kDriveSpeed = 0.8; // GD Lightspeed Circuit Path
-    public static final double kTurnSpeed = 0.7; // GD Lightspeed Circuit Path
+    // public static final double kDriveSpeed = 0.8; // GD Lightspeed Circuit Path
+    // public static final double kTurnSpeed = 0.7; // GD Lightspeed Circuit Path
+    public static final double kDriveSpeed = 1.0;
+    public static final double kTurnSpeed = 0.9;
     public static final double kMaxSpeed = 0.8;
     public static final double kMaxAngularSpeed = Math.PI;
     public static final double kTrackWidth = 0.711;
