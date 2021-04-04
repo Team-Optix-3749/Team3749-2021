cebe8eb5c5a6a909dc33edd9ac60e15bfbf2aff4
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2021-04-03 20:59:46 -0700

    call auto nav instead of other command

--- Team3749-2021/src/main/java/frc/robot/RobotContainer.java
+++ Team3749-2021/src/main/java/frc/robot/RobotContainer.java
@@ -12,8 +12,8 @@ public class RobotContainer {
   private final Drivetrain m_drive = new Drivetrain();
   private final Intake m_intake = new Intake();
   private final Shooter m_shooter = new Shooter();
-  // private final AutoNavPath m_autoCommand = new AutoNavPath(m_drive, Constants.Autonomous.AutoPath);
-  private final AutoTest m_autoCommand = new AutoTest(m_drive);
+  private final AutoNavPath m_autoCommand = new AutoNavPath(m_drive, Constants.Autonomous.AutoPath);
+  // private final AutoSlalomPath m_autoCommand = new AutoSlalomPath(m_drive);
 
   public XboxController m_xboxController = new XboxController(0);
 
