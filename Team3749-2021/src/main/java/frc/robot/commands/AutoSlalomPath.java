9c0c0d76d3c7b7ce445a559d2075142ce01015fe
Raadwan Masum <39575185+raad1masum@users.noreply.github.com>
2021-04-05 19:04:21 -0700

    import pathweaver json slalom

--- Team3749-2021/src/main/java/frc/robot/commands/AutoSlalomPath.java
+++ Team3749-2021/src/main/java/frc/robot/commands/AutoSlalomPath.java
@@ -1,46 +1,142 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.subsystems.Drivetrain;
+// import frc.robot.paths.*;
+
+import java.io.IOException;
+// import java.nio.file.Files;
+import java.nio.file.Path;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 
 /**
+ * An autonomous command to follow a path
+ * 
+ * @author Rohan
+ * @author Raadwan
  */
 public class AutoSlalomPath extends CommandBase {
-  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+  private final Timer m_timer = new Timer();
   private final Drivetrain m_drive;
+  private Trajectory m_trajectory;
+  private final RamseteController m_follower;
+  private DifferentialDriveWheelSpeeds m_prevSpeeds;
+  private double m_prevTime;
 
   /**
-   *
-   * @param Drivetrain The subsystem used by this command.
+   * Creates an ``AutoNavPath`` object
+   * 
+   * @param drivetrain The drivetrain subsystem
+   * @param path       The path to follow
    */
-  public AutoSlalomPath(Drivetrain subsystem) {
-    m_drive = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+  public AutoSlalomPath(Drivetrain drivetrain) {
+    m_drive = drivetrain;
+
+    String trajectoryJSON = "paths/output/slalom.wpilib.json";
+
+    TrajectoryConfig config = new TrajectoryConfig(Units.feetToMeters(10.0), Units.feetToMeters(10.0));
+    config.setKinematics(m_drive.getKinematics());
+
+    // m_trajectory = path.getTrajectory(config);
+
+    try {
+      Path m_trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+      m_trajectory = TrajectoryUtil.fromPathweaverJson(m_trajectoryPath);
+    } catch (IOException ex) {
+      DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+      System.out.println("Unable to open trajectory");
+    }
+
+    m_drive.resetOdometry(m_trajectory.getInitialPose());
+
+    m_follower = new RamseteController(Constants.Autonomous.kB, Constants.Autonomous.kZeta);
+
+    addRequirements(drivetrain);
   }
 
-  // Called when the command is initially scheduled.
+  /**
+   * Runs when the command is initialized
+   */
   @Override
   public void initialize() {
-    m_drive.auto(1.0, 1.0, 0.8); // forward
-    m_drive.auto(-0.7, 0.7, 0.7); // left
+    m_drive.resetOdometry(m_trajectory.getInitialPose());
+    m_prevTime = -1;
+    var initialState = m_trajectory.sample(0);
+    var angularVelocity = initialState.curvatureRadPerMeter * initialState.velocityMetersPerSecond;
+    m_prevSpeeds = m_drive.getKinematics()
+        .toWheelSpeeds(new ChassisSpeeds(initialState.velocityMetersPerSecond, 0, angularVelocity));
+    m_timer.reset();
+    m_timer.start();
+    m_drive.getLeftPIDController().reset();
+    m_drive.getRightPIDController().reset();
+    m_drive.m_leftEncoder.reset();
+    m_drive.m_rightEncoder.reset();
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
+  /**
+   * Runs when the command is executed
+   */
   @Override
   public void execute() {
-    m_drive.stopMotors();
+    double curTime = m_timer.get();
+    double dt = curTime - m_prevTime;
+
+    if (m_prevTime < 0) {
+      m_drive.setOutputVolts(0.0, 0.0);
+      m_prevTime = curTime;
+      return;
+    }
+
+    var targetWheelSpeeds = m_drive.getKinematics()
+        .toWheelSpeeds(m_follower.calculate(m_drive.getPose(), m_trajectory.sample(curTime)));
+
+    var leftSpeedSetpoint = targetWheelSpeeds.leftMetersPerSecond;
+    var rightSpeedSetpoint = targetWheelSpeeds.rightMetersPerSecond;
+
+    var speeds = m_drive.getSpeeds();
+
+    double leftFeedforward = m_drive.getFeedforward().calculate(leftSpeedSetpoint,
+        (leftSpeedSetpoint - m_prevSpeeds.leftMetersPerSecond) / dt);
+
+    double rightFeedforward = m_drive.getFeedforward().calculate(rightSpeedSetpoint,
+        (rightSpeedSetpoint - m_prevSpeeds.rightMetersPerSecond) / dt);
+
+    double leftOutput = leftFeedforward
+        + m_drive.getLeftPIDController().calculate(speeds.leftMetersPerSecond, leftSpeedSetpoint);
+
+    double rightOutput = rightFeedforward
+        + m_drive.getRightPIDController().calculate(speeds.rightMetersPerSecond, rightSpeedSetpoint);
+
+    m_drive.setOutputVolts(leftOutput, -rightOutput);
+    m_prevSpeeds = targetWheelSpeeds;
+    m_prevTime = curTime;
   }
 
-  // Called once the command ends or is interrupted.
+  /**
+   * Runs when the command is stopped
+   */
   @Override
   public void end(boolean interrupted) {
-    m_drive.stopMotors();
+    m_drive.setOutputVolts(0, 0);
+    m_timer.stop();
   }
 
-  // Returns true when the command should end.
+  /**
+   * Returns whether the command has been completed
+   */
   @Override
   public boolean isFinished() {
-    return false;
+    return m_timer.hasElapsed(m_trajectory.getTotalTimeSeconds());
   }
 }
