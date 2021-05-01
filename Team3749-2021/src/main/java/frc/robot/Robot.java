bcebc39dbf49effc56f41cc4658f746d7da5ae40
Raadwan Masum <piraadwan@gmail.com>
2021-04-30 19:13:26 -0700

    make robot container public

--- Team3749-2021/src/main/java/frc/robot/Robot.java
+++ Team3749-2021/src/main/java/frc/robot/Robot.java
@@ -15,7 +15,7 @@ import frc.robot.commands.*;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
-  private static RobotContainer m_robotContainer;
+  public static RobotContainer m_robotContainer;
 
   /**
    * This function is run when the robot is first started up and should be used
